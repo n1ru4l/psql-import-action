b451a1071120885f7c84856f449e41c77d5ddbd6
n1ru4l <laurinquast@googlemail.com>
2020-01-31 12:08:03 +0100

    fix variable name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 PGPASSWORD="$POSTGRES_PASSWORD" psql --host="$POSTGRES_HOST" \
   --port="$POSTGRES_PORT" \
-  --username="$POSTGRES_USERNAME" \
+  --username="$POSTGRES_USER" \
   "$POSTGRES_DATABASE" > "$1"
